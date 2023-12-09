b8522e170657299467e00af6c48b3a612053e245
adimakopoulos <alexdimakopoulos@gmail.com>
2023-12-09 14:21:42 +0200

    second commit

---
+++ Assets/Core/DependenciesManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DependenciesManager : MonoBehaviour
+{
+    Dictionary dependacncies = new Dictionary<string, MonoBehaviour>();
+}
